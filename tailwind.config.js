56fca0c7874c410455e8bf2d22b5c2bfe3020730
CODING-DARSH <darshvit20@gmail.com>
2025-11-09 10:17:06 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-content: ['./index.html', './src/**/*.{js,jsx}'],
-theme: {
-extend: {
-colors: {
-bg: '#0b0b0b',
-panel: '#0a0a0a',
-mid: '#1b1b1b',
-soft: '#6b6b6b'
-}
-}
-},
-plugins: [],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        bg: '#0b0b0b',
+        panel: '#0a0a0a',
+        mid: '#1b1b1b',
+        soft: '#6b6b6b',
+      },
+    },
+  },
+  plugins: [],
 }
